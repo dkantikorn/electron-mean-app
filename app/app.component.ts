dde60f381037a22bb4de1468adadec6aca8feca3
dkantikorn <dk_antikorn@hotmail.com>
2017-09-26 22:59:49 +0700

    project init

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>My First Angular 2 App with Electron</h1>
+  <form>
+  <input type="text" name="xx">
+  <button>Hello</button>
+  `
+})
+export class AppComponent { }
